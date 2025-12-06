588a215a6c5052a82cddf8e7b34add51ef7e7e7c
GN Naane <gnaane@sts3d.co.za>
2025-12-06 21:09:33 +0200

    Abyss theme selected | Daisyui used in theme

--- pizza-pantry/app/layout.tsx
+++ pizza-pantry/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-theme="abyss">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
